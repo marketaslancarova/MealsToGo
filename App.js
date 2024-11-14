bd698221d8afc0a7e9125bbd9ed52d5a98d2aeab
marketaslancarova <marketaslancarova@gmail.com>
2024-11-14 13:53:26 +0100

    RestaurantsScreen with RestaurantInfo components

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, Text, SafeAreaView, View, StatusBar } from "react-native";
 
-import { colors } from "./src/utils/colors";
-
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ExpoStatusBar style="auto" />
-      <View style={styles.search}>
-        <Text>Search</Text>
-      </View>
-      <View style={styles.list}>
-        <Text>List</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <></>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  list: {
-    backgroundColor: colors.GREEN,
-    flex: 1,
-    padding: 16,
-  },
-  search: {
-    backgroundColor: colors.DODGER_BLUE,
-    padding: 16,
-  },
-});
+const styles = StyleSheet.create({});
